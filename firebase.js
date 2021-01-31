efed414fb452375e89af0d932dab2ccafc451560
Chloe <yingyingppd@gmail.com>
2021-01-30 20:49:15 -0800

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBexvu2APnQ056SSb1TvUwXfpsgs6x37xY",
+  authDomain: "signal-c-react-native.firebaseapp.com",
+  projectId: "signal-c-react-native",
+  storageBucket: "signal-c-react-native.appspot.com",
+  messagingSenderId: "339326659209",
+  appId: "1:339326659209:web:95a4524f60af02bbe27458",
+};
